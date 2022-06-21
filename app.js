78cd8904f003a57587f82cf6cc6b1e9dc38ba0a8
sergionoh1257@gmail.com <sergionoh1257@gmail.com>
2022-06-21 15:23:30 -0500

    Added questions

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 let userInputs = [];
 
+//Original Story
 let originalStory = `Once upon a time there was a ${userInputs[0]} year old programmer who was debugging a bug on line ${userInputs[1]} of his code.
 
 He searched that line but found nothing.
@@ -10,9 +11,21 @@ He researched on the Internet, asked in ${userInputs[2]} to be able to solve his
 
 Until he discovered that a ${userInputs[3]} caused an error on line ${userInputs[4]} and that broke the code, he solved it and seeing that it worked, he was happy and went to ${userInputs[5]} to celebrate.`;
 
+// Number of questions and description
 let numberOfQuestions = 6;
+let questionArray = [
+  "enter a age",
+  "enter a number",
+  "enter a Internet Forum name",
+  "enter a type of bug",
+  `enter a number bigger than ${userInputs[1]}`,
+  "enter a action"
+];
 
-
+for (let i = numberOfQuestions; i >= 0; i--)
+{
+  console.log(i);
+}
 
 /* Original Story
 
