bc3a28143de274da40fdddeb43a5d118a6f145ec
sergionoh1257@gmail.com <sergionoh1257@gmail.com>
2022-06-21 15:08:21 -0500

    Added mad-lib replacement variables to story

---
+++ app.js
@@ -0,0 +1,25 @@
+"use strict";
+
+let userInputs = [];
+
+let originalStory = `Once upon a time there was a ${userInputs[0]} year old programmer who was debugging a bug on line ${userInputs[1]} of his code.
+
+He searched that line but found nothing.
+
+He researched on the Internet, asked in ${userInputs[2]} to be able to solve his problem but nobody found his mistake.
+
+Until he discovered that a ${userInputs[3]} caused an error on line ${userInputs[4]} and that broke the code, he solved it and seeing that it worked, he was happy and went to ${userInputs[5]} to celebrate.`;
+
+console.log(originalStory);
+
+/* Original Story
+
+Once upon a time there was a 15 year old programmer who was debugging a bug on line 45 of his code.
+
+He searched that line but found nothing.
+
+He researched on the Internet, asked in forums to be able to solve his problem but nobody found his mistake.
+
+Until he discovered that a semicolon caused an error on line 20 and that broke the code, he solved it and seeing that it worked, he was happy and went to have a coffee to celebrate.
+
+*/
